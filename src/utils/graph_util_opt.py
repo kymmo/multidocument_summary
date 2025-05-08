0f2151c23697cc60bb3463b443b483e15c7ac5bb
zoeee <2508675629@qq.com>
2025-05-08 11:26:05 +0800

    [bugfix] batch size adjust

--- src/utils/graph_util_opt.py
+++ src/utils/graph_util_opt.py
@@ -792,10 +792,10 @@ def get_embedded_pyg_graphs(dataset_type, docs_list, sent_similarity):
                pyg_graph_list_cpu = []
                node_sent_map_list = []
 
-               num_workers = min(3, (auto_workers() // 2) + 1) # for saving memory
+               num_workers = min(2, (auto_workers() // 2) + 1) # for saving memory
                num_items = len(embedded_graph_list)
                if num_items > 0:
-                    batch_size = 100
+                    batch_size = 60
                     with multiprocessing.get_context("spawn").Pool(num_workers) as pool, \
                          tqdm(total=num_items, desc="Converting to PyG") as pbar:
 
